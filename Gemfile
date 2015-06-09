6aaa7f03ef1c6ffa16949309fbadb4d0a5809ecb
Anna Gaughan <agaughan@gmail.com>
2015-06-09 21:20:11 +0100

    more css jazz

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'pry-rails'
   gem 'pry'
   gem 'ransack'
+  gem  'ratyrate'
 
 
 
--- app/models/pt.rb
+++ app/models/pt.rb
@@ -3,4 +3,6 @@ class Pt < ActiveRecord::Base
   has_many :pt_skills
   has_many :skills, through: :pt_skills
   has_many :users
+  has_many :location_pts
+  has_many :locations, through: :location_pts
 end
